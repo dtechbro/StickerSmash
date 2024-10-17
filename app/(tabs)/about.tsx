48cb31e74e888027b93408247cd6911d4b23d799
dtechbro <princeakinola05@gmail.com>
2024-10-17 15:48:27 +0100

    learning'

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -1,21 +1,20 @@
 import { Text, View, StyleSheet } from "react-native";
 
-export default function Index() {
+export default function AboutScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Edit app/index.tsx to edit this screen.</Text>
+      <Text style={styles.text}>About Screen</Text>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#25292e',
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center'
   },
-  
   text: {
     color: '#fff'
   },
